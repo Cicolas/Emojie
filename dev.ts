ba883ebaa1c9f40e0c5c845cba2c3db029434bde
Nícolas dos Santos Carvalho <nicolas.mnw@gmail.com>
2024-06-24 23:32:22 -0300

    dotenv removal

--- dev.ts
+++ dev.ts
@@ -2,12 +2,7 @@
 
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
-import * as dotenv from "https://deno.land/std@0.216.0/dotenv/mod.ts";
 
 import "$std/dotenv/load.ts";
-
-dotenv.loadSync({
-  export: true
-});
 
 await dev(import.meta.url, "./main.ts", config);
